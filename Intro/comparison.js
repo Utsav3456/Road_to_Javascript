399a81b1e004d8b7272568419efaf9912f987352
Utsav3456 <152777010+Utsav3456@users.noreply.github.com>
2025-03-29 16:24:07 +0000

    comaprison

---
+++ Intro/comparison.js
@@ -0,0 +1,11 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// Strict check (===)
+
+console.log("2" == 2);  // Output - true  (because its converting the values into string)
+console.log("2" === 2); // Output - false (because its checking the datatypes of both values)
