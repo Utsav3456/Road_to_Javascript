fb83fe5c526e48f6a7f6a20e422d7cc35df6c516
Utsav3456 <152777010+Utsav3456@users.noreply.github.com>
2025-04-09 16:25:51 +0000

    object 03

---
+++ Data_Structure/objects_03.js
@@ -0,0 +1,19 @@
+const course = {
+    name: "js in hindi",
+    price: "999",
+    teacher: "hitesh"
+};
+
+// course.teacher
+
+const {teacher: a} = course;
+//console.log(teacher);
+console.log(a);
+
+// JSON API
+
+// {
+//     "name": "utsav",
+//     "age": "18"    
+// }
+
